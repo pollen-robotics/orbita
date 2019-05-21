d1816a7e752079142d4b24de446f277e0d7fcb73
Augustin CRAMPETTE <augustin.crampette@laposte.net>
2019-05-21 16:42:58 +0200

    Modify package and module names in README and setup files

--- spherical_joint/setup.py
+++ spherical_joint/setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='spherical-joint',
+    name='spherical_joint',
     version='0.1.0',
     author='Pollen Robotics',
-    packages=['spherical-joint'],
+    packages=['spherical_joint'],
     url='https://github.com/pollen-robotics/spherical-joint',
     license='LICENSE.txt',
     description='Library used to control the spherical actuator',
     install_requires=[
         "numpy",
         "quaternion",
-        "math",
     ],
 )
