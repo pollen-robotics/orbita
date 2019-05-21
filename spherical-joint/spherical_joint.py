b4b13d9a493ef5fd385735bd8d6db59ae5f04feb
Augustin CRAMPETTE <augustin.crampette@laposte.net>
2019-05-21 12:14:51 +0200

    Add a method to get the angles of the disks from a quaternion and a method to reset the last_angles attribute

--- spherical-joint/spherical_joint.py
+++ spherical-joint/spherical_joint.py
@@ -18,6 +18,8 @@ class Actuator:
     y0_quat = quaternion.quaternion(0,0,1,0)
     z0_quat = quaternion.quaternion(0,0,0,1)
 
+    last_angles = [0,2*pi/3,-2*pi/3]
+
     def from_vector_get_new_frame(self,vector,angle=0):
 
         beta = angle*pi/180
@@ -179,7 +181,8 @@ class Actuator:
     def from_quaternion_get_new_frame(self,Q):
         if type(Q)==type((0,0,0)):
             q1 = quaternion.quaternion(Q[0],Q[1],Q[2],Q[3])
-
+        else :
+            q1 = Q
         q1_inv = q1.inverse()
 
 
@@ -192,6 +195,89 @@ class Actuator:
         Z = [new_z.x, new_z.y, new_z.z]
 
         return X,Y,Z
-    #
-    # def from_quaternion_get_angles(self,Q):
-    #     ...
+
+
+    def from_quaternion_get_angles(self,Q):
+        R = self.R
+        Pc = self.Pc_z
+        C = self.Cp_z
+
+        if type(Q)==type((0,0,0)):
+            quat = quaternion.quaternion(Q[0],Q[1],Q[2],Q[3])
+        else : #If type of Q is quaternion
+            quat = Q
+        ### Find q31 and q11 ###
+        X,Y,Z = self.from_quaternion_get_new_frame(quat)
+        q31_ = [2*atan2((R*X[2] - sqrt(R**2*X[2]**2 + R**2*Z[2]**2 - C[2]**2 + 2*C[2]*Pc[2] - Pc[2]**2)),(R*Z[2] + C[2] - Pc[2])), 2*atan2((R*X[2] + sqrt(R**2*X[2]**2 + R**2*Z[2]**2 - C[2]**2 + 2*C[2]*Pc[2] - Pc[2]**2)),(R*Z[2] + C[2] - Pc[2]))]
+
+        if 0<=q31_[0]<=180:
+            q31 = q31_[0]
+        else :
+            q31= q31_[1]
+        num1=Z[1]*cos(q31)+X[1]*sin(q31)
+        den1=Z[0]*cos(q31)+X[0]*sin(q31)
+        q11 = atan2(num1,den1)
+
+        ### Find q32 and q12 ###
+        w_offset = cos(2*pi/6.0)
+        x_offset = sin(2*pi/6.0)*self.z0_quat.x
+        y_offset = sin(2*pi/6.0)*self.z0_quat.y
+        z_offset = sin(2*pi/6.0)*self.z0_quat.z
+
+        q_offset = quaternion.quaternion(w_offset,x_offset,y_offset,z_offset) #1st rotation quaternion
+
+        X,Y,Z = self.from_quaternion_get_new_frame(quat*q_offset)
+        q32_ = [2*atan2((R*X[2] - sqrt(R**2*X[2]**2 + R**2*Z[2]**2 - C[2]**2 + 2*C[2]*Pc[2] - Pc[2]**2)),(R*Z[2] + C[2] - Pc[2])), 2*atan2((R*X[2] + sqrt(R**2*X[2]**2 + R**2*Z[2]**2 - C[2]**2 + 2*C[2]*Pc[2] - Pc[2]**2)),(R*Z[2] + C[2] - Pc[2]))]
+
+        if 0<=q32_[0]<=180:
+            q32 = q32_[0]
+        else :
+            q32= q32_[1]
+        num2=(Z[1]*cos(q32)+X[1]*sin(q32))
+        den2=(Z[0]*cos(q32)+X[0]*sin(q32))
+        q12 = atan2(num2,den2)
+
+        ### Find q33 and q13 ###
+        w_offset = cos(-2*pi/6.0)
+        x_offset = sin(-2*pi/6.0)*self.z0_quat.x
+        y_offset = sin(-2*pi/6.0)*self.z0_quat.y
+        z_offset = sin(-2*pi/6.0)*self.z0_quat.z
+
+        q_offset = quaternion.quaternion(w_offset,x_offset,y_offset,z_offset) #1st rotation quaternion
+
+        X,Y,Z = self.from_quaternion_get_new_frame(quat*q_offset)
+        q33_ = [2*atan2((R*X[2] - sqrt(R**2*X[2]**2 + R**2*Z[2]**2 - C[2]**2 + 2*C[2]*Pc[2] - Pc[2]**2)),(R*Z[2] + C[2] - Pc[2])), 2*atan2((R*X[2] + sqrt(R**2*X[2]**2 + R**2*Z[2]**2 - C[2]**2 + 2*C[2]*Pc[2] - Pc[2]**2)),(R*Z[2] + C[2] - Pc[2]))]
+
+        if 0<=q33_[0]<=180:
+            q33 = q33_[0]
+        else :
+            q33= q33_[1]
+        num3=(Z[1]*cos(q33)+X[1]*sin(q33))
+        den3=(Z[0]*cos(q33)+X[0]*sin(q33))
+        q13 = atan2(num3,den3)
+
+        last_angles = self.last_angles
+
+        if (abs(q11-last_angles[0])>=2.96):
+            if last_angles[0]>0:
+                q11=q11+2*pi
+            elif last_angles[0]<0:
+                q11=q11-2*pi
+        if (abs(q12-last_angles[1])>=2.96):
+            if last_angles[1]>0:
+                q12=q12+2*pi
+            elif last_angles[1]<0:
+                q12=q12-2*pi
+        if (abs(q13-last_angles[2])>=2.96):
+            if last_angles[2]>0:
+                q13=q13+2*pi
+            elif last_angles[2]<0:
+                q13=q13-2*pi
+
+        self.last_angles = [q11,q12,q13]
+
+
+        return [q11*180/pi,(q12*180/pi)-120,(q13*180/pi)+120]
+
+    def reset_last_angles(self):
+        self.last_angles = [0,2*pi/3,-2*pi/3]
