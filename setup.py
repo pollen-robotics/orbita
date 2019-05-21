45a81b061ff8f1e6a1002bec3510464d0656e117
Augustin CRAMPETTE <augustin.crampette@laposte.net>
2019-05-21 15:07:04 +0200

    Add a setup.py file to the package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Spherical_joint',
+    version='0.1.0',
+    author='Pollen Robotics',
+    packages=['spherical_joint'],
+    url='https://github.com/pollen-robotics/spherical-joint',
+    license='LICENSE.txt',
+    description='Library used to control the spherical actuator',
+    install_requires=[
+        "numpy",
+        "quaternion",
+        "math",
+    ],
+)
