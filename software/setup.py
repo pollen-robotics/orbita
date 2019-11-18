c10cae29996bb289203d9dd67f4fe9a6a86489f8
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-11-18 14:06:38 +0100

    Update setup and requirements.

--- software/setup.py
+++ software/setup.py
@@ -1,15 +1,28 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='orbita',
     version='0.1.0',
-    author='Pollen Robotics',
-    packages=['orbita'],
+
+    description='Library used to control the Orbita actuator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
     url='https://github.com/pollen-robotics/spherical-joint',
-    license='LICENSE.txt',
-    description='Library used to control the spherical actuator',
+    author='Pollen Robotics',
+    author_email='contact@pollen-robotics.com',
+
+    packages=find_packages(exclude=['tests']),
+
     install_requires=[
-        "numpy",
-        "pyquaternion"
+        'numpy',
+        'pyquaternion',
         ]
 )
