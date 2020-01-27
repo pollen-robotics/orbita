d43af6905abd2d6224f5e59fbf8b615d698b1f59
Pierre Rouanet <pierre.rouanet@gmail.com>
2020-01-27 16:51:35 +0100

    Update to version 0.2.0

--- software/setup.py
+++ software/setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='orbita',
-    version='0.1.1',
+    version='0.2.0',
 
     description='Library used to control the Orbita actuator',
     long_description=long_description,
