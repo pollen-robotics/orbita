5041f0c4cf1c975f9d297e38f9d715e44e0433eb
Pierre Rouanet <pierre.rouanet@gmail.com>
2022-03-01 15:24:50 +0100

    Implements fake eeprom in flash.

--- firmware/modules/orbita/fake_eeprom.h
+++ firmware/modules/orbita/fake_eeprom.h
@@ -3,7 +3,10 @@
 
 #include <stdint.h>
 
-void read_eeprom(uint32_t addr, uint16_t size, uint8_t *data);
-void write_eeprom(uint32_t addr, uint16_t size, uint8_t *data);
+#include "main.h"
+
+HAL_StatusTypeDef clear_eeprom();
+HAL_StatusTypeDef read_eeprom(uint32_t addr, uint16_t data_size, uint8_t data[]);
+HAL_StatusTypeDef write_eeprom(uint32_t addr, uint16_t data_size, uint8_t data[]);
 
 #endif // FAKE_EEPROM_H
